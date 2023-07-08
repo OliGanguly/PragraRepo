7ca5126b57a354e9147dfbd888116d2901b973ee
Oli Ganguly <oliganguly28@gmail.com>
2023-07-08 13:16:48 -0400

    oli

--- src/Loops/Table.java
+++ src/Loops/Table.java
@@ -18,8 +18,4 @@ public class Table {
         }
     }
 }
-//    int num = 5;
-//        for(int i = 1; i <= 10; ++i)
-//                {
-//                System.out.printf("%d * %d = %d \n", num, i, num * i);
-//                }
+
