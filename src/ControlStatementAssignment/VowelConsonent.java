f5b5df484d1472d4ca63c0a267fd0874bc819470
Oli Ganguly <oliganguly28@gmail.com>
2023-07-13 18:39:18 -0400

    oli

--- src/ControlStatementAssignment/VowelConsonent.java
+++ src/ControlStatementAssignment/VowelConsonent.java
@@ -5,33 +5,41 @@ import java.util.Scanner;
 public class VowelConsonent {
     public static void main(String[ ] arg)
     {
-        String cus_reply;
+        String cus_reply="N";
         do{
             int count=0;
             Scanner sc=new Scanner(System.in);
             System.out.println("Enter a character");
-            char ch=sc.next().charAt(0);
-            switch(ch)
+            String pattern = "[a-z,A-Z]+";
+            if(sc.hasNext(pattern))
             {
-                case  'a' :
-                case 'e'  :
-                case 'i'   :
-                case 'o'  :
-                case 'u'  :
-                case  'A' :
-                case 'E'  :
-                case 'I'   :
-                case 'O'  :
-                case 'U'  : count++;
-
+              String ch=sc.next();
+              if(ch.length()<=1){
+                  switch (ch){
+                      case "a":
+                      case "e" :
+                      case "i":
+                      case "o":
+                      case "u":
+                      case "A":
+                      case "E":
+                      case "I":
+                      case "O":
+                      case "U":count++;
+                  }
+                  if(count==1){
+                      System.out.println("Entered character "+ch+" is  Vowel");
+                  }else{
+                      System.out.println("Entered character " + ch + " is Consonent");
+                  }
+              }else{
+                  System.out.println("Please enter single charater");
+              }
             }
-            if(count==1)
-                System.out.println("Entered character "+ch+" is  Vowel");
-            else {
-                if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
-                    System.out.println("Entered character " + ch + " is Consonent");
-                else
-                    System.out.println("Not an alphabet");
+            else
+            {
+                System.out.println("please enter a valid alphabet");
+                continue;
             }
             System.out.println("Do you want to Exit ? "+"Press Y/y for Exit" + "Press N/n to not exit");
             cus_reply=sc.next();
