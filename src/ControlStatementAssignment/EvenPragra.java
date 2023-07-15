c0995ac5410d14615fcbdefaa0f316949e7e15cc
Oli Ganguly <oliganguly28@gmail.com>
2023-07-15 10:18:43 -0400

    oli

---
+++ src/ControlStatementAssignment/EvenPragra.java
@@ -0,0 +1,16 @@
+package ControlStatementAssignment;
+
+public class EvenPragra {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=1;i<=n;i++){
+         if (i%2==0){
+//             System.out.println(n%2);
+             System.out.println("Even + "+i +"Pragra");
+         }else{
+             System.out.println(i);
+         }
+
+        }
+    }
+}
